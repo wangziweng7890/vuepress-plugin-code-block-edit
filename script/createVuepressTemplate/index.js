5c4276dbbcc32fda4005901581a4343574a0e9af
ypack <ypack@mingyuanyun.com>
2022-05-25 19:31:28 +0800

    fix: 修复脚本失效

--- script/createVuepressTemplate/index.js
+++ script/createVuepressTemplate/index.js
@@ -1,23 +1,22 @@
-const chalk = require('chalk') ;
-const commander = require('commander') ;
-const pkg = require('../../package.json') ;
+#!/usr/bin/env node
+
+const chalk = require("chalk");
+const commander = require("commander");
+const pkg = require("../../package.json");
 const initStandard = require("./standard");
 const { program } = commander;
 
 program
-  .version(pkg.version, '-v --version')
-  .description('create vuepress-docs for your vue-cli proj')
+  .version(pkg.version, "-v --version")
+  .description("create vuepress-docs for your vue-cli proj")
   .action(() => {
-    console.log(chalk.green('Welcome !'));
+    console.log(chalk.green("Welcome !"));
   });
 
-program
-  .command('init')
-  .description('Initial config')
-  .action(initStandard);
+program.command("init").description("Initial config").action(initStandard);
 
-program.on('--help', () => {
-  console.log('no help, just do it!');
+program.on("--help", () => {
+  console.log("no help, just do it!");
 });
 
 program.parse(process.argv);
