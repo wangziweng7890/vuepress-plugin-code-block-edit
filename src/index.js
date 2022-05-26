f48397e6f462350a6ba86aec4a316c22b094550c
ypack <ypack@mingyuanyun.com>
2022-05-26 14:48:09 +0800

    fix: 修复组件引入失败

--- src/index.js
+++ src/index.js
@@ -1,32 +1,43 @@
 /**
  * 提供 ::: demo xxx ::: 语法，用于构建 markdown 中的示例
  */
-const path = require('path');
-const renderDemoBlock = require('./common/render');
-const demoBlockContainers = require('./common/containers');
-import DemoBlock from "./DemoBlock.vue";
-import CodeEdit from "./CodeEdit.vue";
+const path = require("path");
+const renderDemoBlock = require("./common/render");
+const demoBlockContainers = require("./common/containers");
+
+const resolvePath = (p) => path.resolve(__dirname, p).replace(/\\/g, "/");
+
 module.exports = (options = {}, ctx) => {
+  const defaultOpts = {
+    demoBlockComponent: resolvePath("DemoBlock.vue"),
+    codeEditComponent: resolvePath("CodeEdit.vue"),
+  };
+  options = Object.assign(defaultOpts, options);
   return {
     enhanceAppFiles() {
       return {
         name: "dynamic-code",
-        content: `export default ({ Vue }) => { 
-          Vue.component('DemoBlock', '${DemoBlock}');
-          Vue.component('CodeEdit', '${CodeEdit}');
-        }`,
+        content: `
+          export default ({ Vue, router }) => {
+            Vue.component('DemoBlock', () => import('${options.demoBlockComponent}'));
+            Vue.component('CodeEdit', () => import('${options.codeEditComponent}'));
+          }
+         `,
       };
     },
-    chainMarkdown(config) {
-      config
-        .plugin('containers')
-        .use(demoBlockContainers(options))
-        .end();
+    beforeDevServer(app, server) {
+      app.post("/code-edit", function (req, res) {
+        console.log("==========", req, res);
+        res.json({ custom: "response" });
+      });
     },
-    extendMarkdown: md => {
+    chainMarkdown(config) {
+      config.plugin("containers").use(demoBlockContainers(options)).end();
+    },
+    extendMarkdown: (md) => {
       const id = setInterval(() => {
         const render = md.render;
-        if (typeof render.call(md, '') === 'object') {
+        if (typeof render.call(md, "") === "object") {
           md.render = (...args) => {
             const result = render.call(md, ...args);
             const { template, script, style } = renderDemoBlock(result.html);
@@ -37,6 +48,6 @@ module.exports = (options = {}, ctx) => {
           clearInterval(id);
         }
       }, 10);
-    }
+    },
   };
 };
